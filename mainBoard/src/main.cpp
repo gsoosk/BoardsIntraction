0226c3561f4d8451d3570880e008a940b484752d
Farzad Habibi <farzadhabibii98@gmail.com>
2019-02-20 14:27:44 +0330

    This commit will add Software Serial port to main Board

--- mainBoard/src/main.cpp
+++ mainBoard/src/main.cpp
@@ -1,58 +1,52 @@
 #include <Arduino.h>
-/*
-  LiquidCrystal Library - Autoscroll
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch demonstrates the use of the autoscroll()
- and noAutoscroll() functions to make new text scroll or not.
-
- The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystalAutoscroll
-
-*/
-
-// include the library code:
 #include <LiquidCrystal.h>
+#include <AltSoftSerial.h>
+
+// Function Declarations
+void handleDataFromBluetooth();
+void handleDataFromSensorBoard();
+
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+const int bluetoothRate = 9600;
+
+// AltSoftSerial always uses these pins:
+// Transmit  Receive   PWM Unusable
+// --------  -------   ------------
+//  9         8         10
+AltSoftSerial sensorBoardSerial;
+const int sensorBoardPortRate = 9600;
+
+
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(20, 4);
+  
+  // We use Serial port for bluetooth module :
+  Serial.begin(bluetoothRate); // Default communication rate of the Bluetooth module
+
+  // We use Serial port to comunicate with sensor board with rate 9600
+  sensorBoardSerial.begin(sensorBoardPortRate);
 
 }
 
 void loop() {
-    
+  if(Serial.available())
+    handleDataFromBluetooth();
+  if(sensorBoardSerial.available())
+    handleDataFromSensorBoard();  
+}
+
+void handleDataFromBluetooth()
+{
+
+}
+
+void handleDataFromSensorBoard()
+{
+
 }
