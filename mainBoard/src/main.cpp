cd54bd1aa695a496796a0be19c43c648c221285a
Farzad Habibi <farzadhabibii98@gmail.com>
2019-02-20 15:16:21 +0330

    This commit will add showing buffers from sensorboard and bluetooth

--- mainBoard/src/main.cpp
+++ mainBoard/src/main.cpp
@@ -4,7 +4,10 @@
 
 // Function Declarations
 void handleDataFromBluetooth();
+void showBluetoothData();
 void handleDataFromSensorBoard();
+void showSensorBoardData();
+
 
 
 // initialize the library by associating any needed LCD interface pin
@@ -12,14 +15,21 @@ void handleDataFromSensorBoard();
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-const int bluetoothRate = 9600;
+const int BLUETOOTH_RATE = 9600;
+const int BLUETOOTH_BUFF_SIZE = 8;
+char bluetoothBuff[8];
+int bluetoothBuffCurr = 0;
+
 
 // AltSoftSerial always uses these pins:
 // Transmit  Receive   PWM Unusable
 // --------  -------   ------------
 //  9         8         10
 AltSoftSerial sensorBoardSerial;
-const int sensorBoardPortRate = 9600;
+const int SENSOR_BOARD_RATE = 9600;
+const int SENSOR_BOARD_BUFF_SIZE = 8;
+char sensorBoardBuff[SENSOR_BOARD_BUFF_SIZE];
+int sensorBoardBuffCurr = 0;
 
 
 void setup() {
@@ -27,10 +37,10 @@ void setup() {
   lcd.begin(20, 4);
   
   // We use Serial port for bluetooth module :
-  Serial.begin(bluetoothRate); // Default communication rate of the Bluetooth module
+  Serial.begin(BLUETOOTH_RATE); // Default communication rate of the Bluetooth module
 
   // We use Serial port to comunicate with sensor board with rate 9600
-  sensorBoardSerial.begin(sensorBoardPortRate);
+  sensorBoardSerial.begin(SENSOR_BOARD_RATE);
 
 }
 
@@ -43,10 +53,28 @@ void loop() {
 
 void handleDataFromBluetooth()
 {
-
+  bluetoothBuff[bluetoothBuffCurr] = Serial.read();
+  bluetoothBuffCurr ++;
+  if(bluetoothBuffCurr == BLUETOOTH_BUFF_SIZE)
+    showBluetoothData();
+}
+void showBluetoothData()
+{
+  lcd.setCursor(0, 0);
+  lcd.print(bluetoothBuff);
+  bluetoothBuffCurr = 0;
 }
 
 void handleDataFromSensorBoard()
 {
-
+  sensorBoardBuff[sensorBoardBuffCurr] = Serial.read();
+  sensorBoardBuffCurr ++;
+  if(sensorBoardBuffCurr == SENSOR_BOARD_BUFF_SIZE)
+    showSensorBoardData();
+}
+void showSensorBoardData()
+{
+  lcd.setCursor(0, 1);
+  lcd.print(sensorBoardBuff);
+  sensorBoardBuffCurr = 0;
 }
