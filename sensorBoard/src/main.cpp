5dbcaac33ed85bb881c93e66393e17328a403339
Ssadaf <s.sadaf2277@gmail.com>
2019-02-20 17:38:05 +0330

    This commit will edit sensor board arduino code.

--- sensorBoard/src/main.cpp
+++ sensorBoard/src/main.cpp
@@ -5,10 +5,10 @@
 #define TRIG_PIN 13
 #define ECHO_PIN 12
 
-double tempVal;
-long duration, cm;
-
 LM35 tempSensor(TEMP_PIN);
+
+int readDistance();
+String encodeData(float tempreture, long distance);
 
 void setup() {
   Serial.begin(9600);
@@ -18,30 +18,42 @@ void setup() {
 }
 
 void loop() {
-  tempVal = tempSensor.cel();
+  double tempreture = tempSensor.cel();
+  long distance = readDistance();
 
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  String msg = encodeData(tempreture, distance);
+  
+  char* buffer = (char*) malloc(sizeof(char)*msg.length()+1);
+  msg.toCharArray(buffer, msg.length()+1);
+  
+  //Serial.write(buffer);
+  Serial.println(buffer);
+  delay(200);
+}
+
+int readDistance(){
+  long duration, cm;
+ 
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
   digitalWrite(TRIG_PIN, LOW); 
   delayMicroseconds(2); 
   digitalWrite(TRIG_PIN, HIGH); 
   delayMicroseconds(10); 
   digitalWrite(TRIG_PIN, LOW);
-  
-  // // Read the signal from the sensor: a HIGH pulse whose
-  // // duration is the time (in microseconds) from the sending
-  // // of the ping to the reception of its echo off of an object.
+ 
+  // Read the signal from the sensor: a HIGH pulse whose duration is the time (in microseconds) from the sending of the ping to the reception of its echo off of an object.
   duration = pulseIn(ECHO_PIN, HIGH);
  
-  // // Convert the time into a distance
-  cm = duration/58; 
   // The speed of sound is 340 m/s or 29 us per cm.The Ultrasonic burst travels out & back.So to find the distance of object we divide by 58 
-  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  cm = ((duration/2) / 29) + 1;    
+  return cm;
+}
 
-  String tempStr = String(tempVal);
-  String distStr = String(cm);
+String encodeData(float tempreture, long distance){
+  String tempStr = String(tempreture);
+  String distStr = String(distance);
+  
   String msg = tempStr + ":" + distStr + "#";
-  char* buffer = (char*) malloc(sizeof(char)*msg.length()+1);
-  msg.toCharArray(buffer, msg.length()+1);
-  Serial.write(buffer);
+  
+  return msg;
 }
