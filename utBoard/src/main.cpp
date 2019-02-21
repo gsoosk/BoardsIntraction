922daed780f0980d42b50757a1c87d0d93507a71
Yasaman <ys.jafari98@gmail.com>
2019-02-21 20:10:16 +0330

    This commit will calculate the sigma of previous results in UTBoard.

--- utBoard/src/main.cpp
+++ utBoard/src/main.cpp
@@ -24,6 +24,7 @@ union {
 } uData;
 
 void setup() {
+  float result = 0;
   Wire.begin();        
   Serial.begin(9600); 
   turnOnSensor(); 
@@ -33,7 +34,7 @@ void loop() {
   float x = getDataFromSensor(X_REG_0_ADDR, X_REG_1_ADDR, X_REG_2_ADDR, X_REG_3_ADDR);
   float y = getDataFromSensor(Y_REG_0_ADDR, Y_REG_1_ADDR, Y_REG_2_ADDR, Y_REG_3_ADDR);
 
-  float result = x*x + y*y;
+  float result = result + x*x + y*y;
 
   Serial.print(result, 17);
   delay(100);
